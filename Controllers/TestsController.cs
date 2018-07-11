d2d7dc2d6519c932c7cf9d95a2304d70185392b8
Vincent Hetet <vincenthetet@Vincent-Hetets-MacBook-Pro.local>
2018-07-11 10:18:58 -0500

    add dumny test to configure test setup

---
+++ Controllers/TestsController.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace vega.Controllers
+{
+    public class TestsController
+    {
+        [Fact]
+        public void TestName()
+        {
+            Assert.True(true);
+        }
+    }
+}
